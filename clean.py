b1f41f18d45a05e82754048d55bf75284daadf2a
Benjamin Clos <benjamin.clos@gmail.com>
2015-11-05 09:32:51 -0500

    fix project structure
    changes project structure

--- clean.py
+++ clean.py
@@ -1,23 +1,29 @@
 import json
 
 extraneous_keys = ['position', 'meta', 'sid', 'updated_meta', 'created_meta']
+numeric_keys    = ['count', 'percent', 'year']
 
 def to_dict_without(keys_to_ignore, keys, vals):
   proto = {}
   for i, key in enumerate(keys): 
-    if not key in keys_to_ignore: proto[key] = vals[i]
+    if not key in keys_to_ignore:
+      proto[key] = vals[i]
   return proto
 
+def data_dir(filename):
+  return "./data/" + filename
 
-with open('./new-york-most-popular-ways-to-die.json') as data:
+with open( data_dir('nyc-popular-deaths.gov.json') ) as data:
   nyc = json.load(data)
 
 lower_case_cols = [ mixedCaseKey.lower() for mixedCaseKey in [ col['name'] for col in nyc['meta']['view']['columns'] ] ]
+
+print(lower_case_cols)
 
 deaths = [ to_dict_without(extraneous_keys, lower_case_cols, row) for row in nyc['data'] ]
 
 deaths_by_count = sorted( deaths, key=lambda row: row.get('count'), reverse=True )
 
-with open('nyc-deaths-by-count.json', 'w') as output:
+with open( data_dir('cleaned.json') , 'w') as output:
   output.write( json.dumps( deaths_by_count, indent=2) )
   output.close()
